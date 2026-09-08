885f113bd511771dcf0b7a2ce3dae8906b0dae2e
s94130881-dev <s94130881@gmail.com>
2026-09-08 10:41:13 -0300

    Create SystemInstructions.cs

---
+++ Instructions/SystemInstructions.cs
@@ -0,0 +1,10 @@
+namespace Nexora.CPU.Instructions;
+
+public static class SystemInstructions
+{
+    public static void Execute()
+    {
+        throw new NotImplementedException(
+            "ARM64 System instruction ainda não implementada.");
+    }
+}
