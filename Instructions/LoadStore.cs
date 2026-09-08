aae369c0eed26b9bfa9e2d93a4b2c5e022920d38
s94130881-dev <s94130881@gmail.com>
2026-09-08 10:40:46 -0300

    Create LoadStore.cs

---
+++ Instructions/LoadStore.cs
@@ -0,0 +1,10 @@
+namespace Nexora.CPU.Instructions;
+
+public static class LoadStore
+{
+    public static void Execute()
+    {
+        throw new NotImplementedException(
+            "ARM64 Load/Store ainda não implementado.");
+    }
+}
